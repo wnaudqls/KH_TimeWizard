2d95a9cb36f5eecf075a51470b87ce1bafbd69fe
sanheekim <68312957+sanheekim@users.noreply.github.com>
2020-10-21 23:38:15 +0900

    (일반) 로그인 세션 저장, 로그아웃 시 세션 제거, 로그인 아이디/pw 미입력 혹은 입력 오류 시 에러 팝업 알림
    (일반) 로그인 세션 저장, 로그아웃 시 세션 제거, 로그인 아이디/pw 미입력 혹은 입력 오류 시 에러 팝업 알림

--- NaverLoginTry/src/main/java/com/minibean/timewizard/controller/LoginController.java
+++ NaverLoginTry/src/main/java/com/minibean/timewizard/controller/LoginController.java
@@ -1,27 +1,28 @@
 package com.minibean.timewizard.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.minibean.timewizard.model.biz.UserInfoBiz;
-import com.minibean.timewizard.model.dao.UserInfoDao;
 import com.minibean.timewizard.model.dto.UserInfoDto;
 import com.minibean.timewizard.utils.LoginNaverVO;
 
@@ -52,22 +53,43 @@ public class LoginController {
 		
 		return "userlogin";
 	}
-	
-	@RequestMapping(value="/general", method= {RequestMethod.GET, RequestMethod.POST})
-	public String generalLogin(UserInfoDto dto, HttpSession session) {
-		logger.info(">> [CONTROLLER-USERINFO] general login");
 		
-		UserInfoDto result = null; 
-		try { 
-		result = userInfoBiz.selectOne(dto); 
-		session.setAttribute("login", result); 
-		return "redirect:../success"; 
-		} catch (Exception e) { 
-		logger.info("[ERROR] CONTROLLER-USERINFO :: general login"); 
-		e.printStackTrace(); 
-		} 
-		return "redirect:../"; 
-
+	// 일반 로그인 ID 혹은 PW를 입력하지 않았거나 틀렸을 때 (userlogin.jsp의 javascript와 연결)
+	@RequestMapping(value="/ajaxlogin", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Boolean> ajaxLogin(@RequestBody UserInfoDto dto, HttpSession session){
+		logger.info("[ajaxlogin]");
+		
+		UserInfoDto res = userInfoBiz.selectOne(dto);
+		
+		boolean check = false;
+		if (res != null) {
+			// 로그인 값을 계속 가지고 있는 Session
+			session.setAttribute("login", res);
+			check = true;
+		}
+		
+		Map<String, Boolean> map = new HashMap<String, Boolean>();
+		map.put("check", check);
+		
+		return map;
+	}
+	
+	// 일반 로그인에서 아이디/PW를 맞게 입력했을 때 넘어감
+	@RequestMapping(value="/success")
+	public String success(UserInfoDto dto, HttpSession session) {
+		
+		return "redirect:../success";
+		
+	}
+	
+	// 로그아웃 시 세션 제거
+	@RequestMapping(value="/logout")
+	public String logout(UserInfoDto dto, HttpSession session){
+		
+		session.invalidate();
+		return "index";
+		
 	}
 	
 	@RequestMapping(value="/navercallback", method= {RequestMethod.GET, RequestMethod.POST})
@@ -81,7 +103,7 @@ public class LoginController {
 		JsonObject object = JsonParser.parseString(apiResult).getAsJsonObject().get("response").getAsJsonObject();
 		
 		UserInfoDto naverdto = new UserInfoDto();
-//		System.out.println(object.get("id").toString().split("\"")[1]);
+		//		System.out.println(object.get("id").toString().split("\"")[1]);
 		// "id" 식으로 가져온다!
 		
 		naverdto.setUser_id(object.get("id").toString().split("\"")[1]);
@@ -126,13 +148,6 @@ public class LoginController {
 			return "redirect:../login";
 		}
 		
-	}
-	
-	@RequestMapping(value="/logout")
-	public String logout(UserInfoDto dto, HttpSession session){
-		
-		session.invalidate();
-		return "../index.jsp";
 	}
 	
 }
