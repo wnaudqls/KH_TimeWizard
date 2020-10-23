4e93fba678776a63738176f935df5359d09314aa
yoonyongmin <dydalszhffk@naver.com>
2020-10-23 14:36:50 +0900

    main(stopwatch)

---
+++ TimeWizard/src/main/java/com/minibean/timewizard/controller/StopwatchController.java
@@ -0,0 +1,19 @@
+package com.minibean.timewizard.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StopwatchController {
+	
+	private Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping("/stopwatch")
+	public String Stopwatch() {
+		logger.info("[stopwatch]");
+		return "stopwatch";
+	}
+
+}
