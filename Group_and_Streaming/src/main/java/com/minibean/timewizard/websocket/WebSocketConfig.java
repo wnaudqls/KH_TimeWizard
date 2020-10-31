8eb7892d32c963cf9593cc4d039ad6246fce2676
wnaudqls <68018111+wnaudqls@users.noreply.github.com>
2020-10-31 13:12:35 +0900

    알림작업
    알림작업중

--- Group_and_Streaming/src/main/java/com/minibean/timewizard/websocket/WebSocketConfig.java
+++ Group_and_Streaming/src/main/java/com/minibean/timewizard/websocket/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.minibean.timewizard.websocket;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
