ff8f4a381ee08ddb553aa63897f5c8920110753d
wnaudqls <68018111+wnaudqls@users.noreply.github.com>
2020-11-15 16:07:16 +0900

    댓글 수정기능
    댓글 수정기능

--- TimeWizard/src/main/webapp/resources/js/notice_reply.js
+++ TimeWizard/src/main/webapp/resources/js/notice_reply.js
@@ -1,6 +1,7 @@
 // 사용자가 답변 버튼을 클릭했을 때
 // 서버로 바로 데이터가 날라가지 않도록 하는 기능
 	var notice_no = document.getElementById("nno").value;
+	var user_no = document.getElementById("user_no").value;
 
 	function showReplyList(){
 
@@ -38,12 +39,14 @@
 						var redate = result[i].reply_regdate;
 						var uid = result[i].user_id;
 						var user_name = result[i].user_name;
+						var rec = '\''+result[i].reply_content+'\'';
+						var uname = '\''+result[i].user_name+'\'';
 						
-						console.log(uid);
+						console.log(result[i].user_no);
 						
 	                    /* htmls += '<div class="media text-muted pt-3" id="reply_no' + this.reply_no + '">';*/
 						
-						 htmls += '<div class="relist">'
+						 htmls += '<div class="relist'+reno+'">';
 						 htmls += '<div> ';
 	                     htmls += '<p>';
 	                     htmls += '<span class="d-block">';
@@ -51,8 +54,10 @@
 	                     htmls += '<p>Comment &nbsp; : &nbsp; '+recon +'</p>';
 	                     htmls += '<h5>Date &nbsp; : '+ redate+'</h5>';
 	                     htmls += '<span style="padding-left: 7px; font-size: 9pt">';
-	                     htmls += '<a href="javascript:void(0)" onclick="fn_editReply(' + reno + ', \'' + uno + '\', \'' + recon + '\' )" style="padding-right:5px">수정</a>';
-	                     htmls += '<a href="javascript:void(0)" onclick="fn_deleteReply(' + reno + ')" >삭제</a>';
+						 if(uno == user_no){
+	                     	htmls += '<a href="javascript:void(0)" onclick="fn_editReply(' + reno + ',' + uno +','+ rec +','+ uname +' )" style="padding-right:5px">수정</a>';
+	                     	htmls += '<a href="javascript:void(0)" onclick="fn_deleteReply(' + reno + ')" >삭제</a>';
+						 }
 	                     htmls += '</span>';
 	                     htmls += '</span>';
 	                     htmls += '</p>';
