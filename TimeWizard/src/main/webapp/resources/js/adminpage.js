c075c0d1e8cb30e41aa709787bab001a86b79b7d
sanheekim <68312957+sanheekim@users.noreply.github.com>
2020-11-16 15:44:59 +0900

    관리자페이지 강제탈퇴 alert 진행 중
    관리자페이지 강제탈퇴 alert 진행 중

--- TimeWizard/src/main/webapp/resources/js/adminpage.js
+++ TimeWizard/src/main/webapp/resources/js/adminpage.js
@@ -1,4 +1,18 @@
-$(document).on("click","button[name='disablebtn']",function(){
+/* $(document).on("click","button[name='disablebtn']",function(){
 	var num = $(this).data('value');
 	$("#userdelete").val(num);
-});
+}); */
+
+function button_event(){
+
+	if(confirm('회원 아이디를 비활성화 하겠습니까?')==true){
+		
+		document.userbye.submit();
+		alert("비활성화를 성공하셨습니다.");
+		
+	} else {
+		
+		alert("비활성화를 취소하셨습니다.");
+		
+	}
+};
